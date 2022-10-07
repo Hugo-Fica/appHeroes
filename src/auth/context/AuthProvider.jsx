8c671f2eee15076295bedd81dc841caf17abd5bb
Hugo Fica <hugo.fica.saez@gmail.com>
2022-10-07 07:02:35 -0300

    update

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,11 +1,19 @@
+import { useReducer } from "react"
 import { AuthContext } from "./AuthContext"
+import { authReducer } from "./AuthReducer"
 
 
-export const AuthProvider = ({children}) => {
-    
+const initialState = {
+  logged: false,
+};
+
+export const AuthProvider = ({ children }) => {
+
+  const [state, dispatch] = useReducer(authReducer, initialState);
+
   return (
     <AuthContext.Provider value={{}}>
-        {children}
+      {children}
     </AuthContext.Provider>
   )
 }
