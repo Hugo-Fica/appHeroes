36300ec44da5519c411283b44d4ccd04c16c7fa7
Hugo Fica <hugo.fica.saez@gmail.com>
2022-10-08 22:35:55 -0300

    update

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import { useReducer } from "react"
+import { types } from "../types/types";
 import { AuthContext } from "./AuthContext"
 import { authReducer } from "./AuthReducer"
 
@@ -7,12 +8,44 @@ const initialState = {
   logged: false,
 };
 
+const init = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return {
+    logged: !!user,
+    user,
+  }
+}
+
 export const AuthProvider = ({ children }) => {
 
-  const [state, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, initialState, init);
+
+  const login = (name = '') => {
+    const user = { id: 'ABC', name }
+    const action = {
+      type: types.login,
+      payload: user
+    }
+
+    localStorage.setItem('user', JSON.stringify(user));
+    dispatch(action);
+  }
+  const logout = () => {
+    const action ={
+      type: types.logout,
+      
+    }
+    localStorage.removeItem('user');
+    dispatch(action);
+  }
 
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={{
+      ...authState,
+      login,
+      logout
+    }}>
       {children}
     </AuthContext.Provider>
   )
